52774f238d89d915b46584068d118993fc24d8b4
Andrey Nelepov <nelepow@gmail.com>
2019-03-22 18:31:03 -0400

    working on displaying guessed letters

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,53 +1,72 @@
 
 // 1.Create and array to hold the presidents of big 8
-var game = ['putin', 'trump','merkel'];
-
+var game = ['trump','merkel', 'putin'];
 var s;
-var count = 0;
-var attemptsLeft = 10 - count;
+var count = 0;//for letters guessed checking
+var attemptsLeft = 11;
 
+var randomNum = Math.floor(Math.random() * game.length);
+var randomWord = game[randomNum];
+var remainingLetters = randomWord.length;
+//var win = randomWord;
+var winScore = 0;
 //Empty array to store the guesses
 var answerArray = [];
+//var lettersGuessedArr = [];
+//var l;
+var output = '';
 
 
-//picking a random word from an array with keydown function
-function runGame(event) {
-  if (event.keyCode === 13) {
-    var randomWord = game[Math.floor(Math.random() * game.length)]; 
-    console.log(randomWord);
+   document.addEventListener("keyup", function (event) {
+    
     //filling the answerArr with inderscores
 //the number of underscores matches the random Word 
     for (var i = 0; i < randomWord.length; i++) {
       answerArray[i] = '_ ';
     }
     //puting them in a string
-    s = answerArray.join('');
-    document.getElementById('currentWord').innerHTML = s;
-   }
-  }
+    s = answerArray.join(' ');
+    document.getElementById('randomWord').innerHTML = s;
+   });
+
+  
+     document.addEventListener("keyup", function() {
+      //here we get the letter that the user typed in the box
+       var keyNum = event.keyCode;
+       var letter = String.fromCharCode(keyNum).toLowerCase();
+       console.log(letter);
+       //making sure there is a guess and letters lower case used
+       if (event.keyCode >= 97 && event.keyCode <= 122) {
+        // if the randomWord contains a letter that user typed in
+        for (var i = 0; i < randomWord.length; i++) {
+          if (randomWord[i] === letter) {
+            // assigning this letter 
+            answerArray[i] = letter;
+          }
+          
+      }
+        remainingLetters--;
+        var output = output + letter + ' ';
+        document.querySelector('#randomWord').innerHTML = output;
+        console.log(remainingLetters);
+         
+     }
+     
+        attemptsLeft--;
+        document.getElementById('attemptsLeft').innerHTML = attemptsLeft;
+       
+     });
   
 
+// //when user guesed the word;
+// if (remainingLetters = 0) {
+//   alert('You Win! ' + "The Leader Name is " + s.toUpperCase);
+//   answerArray[j].classList.add('winScore');
+//   winScore++;
+//   document.querySelector('#winScore').innerHTML = winScore;
+//   winScore.classList.add('winScore');
 
-
-// function Letter() {
-//   //here we get the letter that the user typed in the box
-//   var letter = document.getElementById('letter').value;
-
-//   if (letter.length > 0) {
-//     //if the randomWord contains a leeter that user typed in
-//     for (var j=0; j < randomWord.length; j++) {
-//       if (randomWord[j] === letter) {
-//         // assigning this letter 
-//         answerArray[j] = letter;
-//       }
-//     }
-//     //how many times it takes to guess
-//     count++
-//     document.getElementById('counter').innerHTML = "No of clicks: " + count;
-//     document.getElementById('numberGuesses').innerHTML = attemptsLeft;
-//   }
+//   break;
 // }
 
-
-
-
+// letter.length > 0 &&
